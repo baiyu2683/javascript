3fb921d182a2071c2965ceb1c1048947d66bef3b
zhangheng <zhangheng2683@gmail.com>
2016-07-15 00:01:49 +0800

    第六章一些枚举属性的练习

--- 第六章/app.js
+++ 第六章/app.js
@@ -29,4 +29,39 @@ function show2(){
     "use strict";
     Object.prototype = 0;
 }
-
+/*
+ *将p中可枚举的属性复制到o中，
+ *如果o和p中含有同名属性，则覆盖o中属性
+ *不处理getter和setter以及复制属性
+ */
+function extend(o,p){
+    for(prop in p){
+        o[prop] = p[prop];
+    }
+    return o;
+}
+/*合并*/
+function merge(o,p){
+    for(prop in p){
+        if(!o.hasOwnProperty(prop)){
+            o[prop] = p[prop];
+        }
+    }
+    return o;
+}
+/*删除非同名属性*/
+function restrict(o, p){
+    for(prop in o){
+        if(! (prop in p)){
+            delete o[prop];
+        }
+    }
+    return o;
+}
+/*删除同名属性*/
+function substract(o, p){
+    for(prop in p){
+        delete o[prop];
+    }
+    return o;
+}
