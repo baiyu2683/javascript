a24ea9d2b192b9702fc1838d3ec25682e64892fd
zhangheng <zhangheng2683@gmail.com>
2016-08-25 23:27:07 +0800

    let，这个node版本不支持，暂时放弃

--- 第十一章/TestLet.js
+++ 第十一章/TestLet.js
@@ -1,3 +1,5 @@
+'use strict';
+
 function oddsums(n){
     let total = 0, result = [];
     for(let x = 1; x <= n; x++){
@@ -16,8 +18,18 @@ let o = {x:1, y:2};
 for(let p in o){
     console.log(p);
 }
+//以下代码貌似都不对，跳过这些介绍了。。之后学es6和nodejs怎么支持es6
 
- for each (let v in o){
-    console.log(v);
-}
+ //for each (let v in o){    //会报错
+   // console.log(v);
+//}
 //console.log(p);
+
+let x = 1;
+for(let x = x + 1; x < 5; x++){  //报错 ReferenceError: x is not defined
+    console.log(x);
+}
+{
+    let x = x + 1;
+    console.log(x);
+}
