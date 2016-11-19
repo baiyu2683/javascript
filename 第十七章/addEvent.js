cdec5729c486bdc67a2b27f8f8c87f6952876022
zhangheng <zhangheng2683@gmail.com>
2016-11-19 10:11:04 +0800

    多了一个分号

--- 第十七章/addEvent.js
+++ 第十七章/addEvent.js
@@ -6,7 +6,7 @@ function addEvent(target, type, handler){
     if(target.addEventListener)
         target.addEventListener(type, handler, false);
     else
-        target.attachEvent("on" + type, ,
+        target.attachEvent("on" + type,
                            function(event){
                                //把处理程序作为事件目标的方法调用
                                //传递事件对象
