7f1967a0857120940b0aca82061d22e4c34e4f97
zhangheng <zhangheng2683@gmail.com>
2016-11-22 23:49:19 +0800

    +发送同步请求

--- 第十八章/getText.js
+++ 第十八章/getText.js
@@ -1,3 +1,4 @@
+//发送一个get请求到制定url
 function getText(url, callback){
     url = url || "http://localhost:9006/test/get1";
     callback = callback || show;
@@ -17,4 +18,22 @@ function getText(url, callback){
 
 function show(msg) {
     console.info(msg);
+}
+
+//发送同步的HTTP get请求
+//返回指定的文本，如果请求不成功或响应不是文本就报错
+function getTextSync(url) {
+    var request = new XMLHttpRequest();
+    request.open("GET", url, false);        //false表示方法是同步的
+    request.send(null);  //立即发送请求
+    //如果请求不是200 OK，就报错
+    if (request.status !== 200) throw new Error(request.statusText);
+
+    //如果是类型错误，就报错
+    var type = request.getResponseHeader("Content-Type");
+    if(!type.match(/text|json/)) {
+        throw new Error("Expected textual or json response; got: " + type);
+    }
+
+    return request.responseText;
 }
